40a8ddd29feac519e3043fb00d8268cf4ae2879a
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-07-24 15:35:39 +0600

    added

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-center text-2xl font-bold ">Netflix clone</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
