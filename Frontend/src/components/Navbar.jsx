8fe09b3b244daa73875ca4b0068aac8c56a3a461
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-07-24 18:19:26 +0600

    added

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,14 +1,29 @@
+import { IoSearch } from "react-icons/io5";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
-import { FaArrowUp } from "react-icons/fa";
-
+import logo from "../assets/logo.png"
 const Navbar = () => {
     return (
         <div>
-            <button>
-                <FaArrowUp />
+            <div className="navbar flex justify-between items-center p-4">
+                <div className="Left-navbar flex items-center  w-1/2 justify-between p-2">
+                    <img src={logo} alt="Logo" className="w-15 h-8" />
 
-            </button>
-            <h1>Navbar Component</h1>
+                    <ul className="flex items-center gap-2  ">
+
+                        <li>Home</li>
+                        <li>TV Shows</li>
+                        <li>Movies</li>
+                        <li>My List</li>
+                        <li>Series</li>
+                    </ul>
+                </div>
+                <div className="Right-nav flex items-center gap-6 cursor-pointer" >
+                    <IoSearch />
+                    <IoMdNotificationsOutline />
+
+                </div>
+            </div>
         </div>
     )
 }
