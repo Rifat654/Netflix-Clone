9c71ae82e23a0b5ffbf00712b97f3a9af2eb659b
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-08-04 17:26:02 +0600

    adding arrow icon on top of the tittle card

--- Frontend/src/components/Tittle_card.jsx
+++ Frontend/src/components/Tittle_card.jsx
@@ -1,13 +1,21 @@
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
 
 import { cards_data } from "../Data/data.js";
 const Tittle_card = () => {
 
     return (
-        <div className="w-full h-full bg-black text-white px-8 py-8">
-            <div>
-                <h2 className="text-2xl text-left-50 font-bold mb-4">Trending Movies</h2>
+        <div className="w-full h-full bg-black text-white px-2 md:px-8 py-8">
+
+            <div className="text-right-100 flex items-center justify-between px-3">
+                <h2 className="text-xl md:text-2xl  font-bold mb-4">Trending Movies</h2>
+                <div className="flex items-center gap-2 text-lg md:text-xl cursor-pointer">
+                    <FaArrowLeft />
+                    <FaArrowRight />
+                </div>
             </div>
-            <div className="flex gap-4 px-10 overflow-x-auto flex-nowrap hide-scrollbar ">
+
+            <div className="flex gap-4 px-3 overflow-x-auto flex-nowrap hide-scrollbar ">
 
                 {cards_data.map((card, index) => (
                     <div
@@ -17,7 +25,7 @@ const Tittle_card = () => {
                         <img
                             src={card.image}
                             alt={card.name}
-                            className="w-full h-72 object-cover rounded-md"
+                            className="w-full h-30 md:h-40 object-cover rounded-md"
                         />
 
                         <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center rounded-md">
